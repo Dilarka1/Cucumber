ae87434f41a22dfba2970842d72d0e57640d0227
Dilarka1 <dilarakhafizova@gmail.com>
2020-02-15 23:29:10 -0500

    day 4

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@login_with_credentials_ddt",
+        tags = "@create_car",
         plugin = {"html:target/default-cucumber-reports",
         "json:target/cucumber.json"
         }
--- src/test/java/com/vytrack/step_definitions/TopMenuStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/TopMenuStepDefinitions.java
@@ -12,22 +12,4 @@ public class TopMenuStepDefinitions {
     public void user_navigates_to_then_to(String module, String submodule) {
         loginPage.navigateTo(module, submodule);
     }
-
-    @Then("user click on {string} button")
-    public void user_click_on_button(String string) {
-
-    }
-
-    @Then("user adds new car information:")
-    public void user_adds_new_car_information(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-
-    }
-
 }
