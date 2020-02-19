b4c577450797ffbc2c6982205db86a091898fc1f
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-02-19 10:47:42 -0500

    new edit

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -1,8 +1,5 @@
 package com.vytrack.utilities;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.log4j.Logger;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,34 +8,15 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
-
-import java.net.URL;
-
-
 public class Driver {
-    private static Logger logger = Logger.getLogger(Driver.class);
     private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
-
-    // with ThreadLocal it is not singleton anymore, now it's ThreadLocal Driver
-    // it will duplicate by itself
-    //it looks like singleton, but different. Webdriver object will be duplicated at the runtime.
-    //by default it will run only one browser
-    //it will return you instance thread id
     private Driver() {
-
     }
-
-    //if switch statement complains on string parameter
-    //change java version to 7+, better at least 8
-    //File--> Project Structure-->Set project language level to at least 8 or above
     public static WebDriver get() {
         //if this thread doesn't have a web driver yet - create it and add to pool
         if (driverPool.get() == null) {
-            logger.info("TRYING TO CREATE DRIVER");
+            System.out.println("TRYING TO CREATE DRIVER");
             // this line will tell which browser should open based on the value from properties file
             String browserParamFromEnv = System.getProperty("browser");
             String browser = browserParamFromEnv == null ? ConfigurationReader.getProperty("browser") : browserParamFromEnv;
@@ -80,33 +58,11 @@ public class Driver {
                     WebDriverManager.getInstance(SafariDriver.class).setup();
                     driverPool.set(new SafariDriver());
                     break;
-                case "remote_chrome":
-                    try {
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName(BrowserType.CHROME);
-                        desiredCapabilities.setCapability("platform", Platform.ANY);
-                        driverPool.set(new RemoteWebDriver(new URL("http://ec2-18-212-156-23.compute-1.amazonaws.com:4444/wd/hub"), desiredCapabilities));
-                    } catch (Exception e) {
-                        logger.error(e.getMessage());
-                        e.printStackTrace();
-                    }
-                    break;
-                case "remote_firefox":
-                    try {
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName(BrowserType.FIREFOX);
-                        driverPool.set(new RemoteWebDriver(new URL("http://ec2-18-212-156-23.compute-1.amazonaws.com:4444/wd/hub"), desiredCapabilities));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    break;
             }
-
         }
         //return corresponded to thread id webdriver object
         return driverPool.get();
     }
-
     public static void close() {
         driverPool.get().quit();
         driverPool.remove();
